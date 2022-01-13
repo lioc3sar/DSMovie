ff6c84dbbef69e0014bc758206943bd09d57edbf
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-01-12 21:03:11 -0300

    MovieCard

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -1,9 +1,36 @@
-import Pagination from "pages/pagination";
+import MovieCard from "components/movie-card";
+import Pagination from "components/pagination";
 
-function Listing(){
-    return(
-        <Pagination />
-    )
+function Listing() {
+  return (
+    <>
+      <Pagination />
+      <div className="container">
+        <div className="row">
+          <div className="col col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div>
+          <div className="col col-sm-6 col-lg-4 mb-3">
+            <MovieCard />
+          </div>
+          
+        </div>
+      </div>
+     
+    </>
+  );
 }
 
-export default Listing
+export default Listing;
