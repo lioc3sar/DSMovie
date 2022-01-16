c78e7fbc2e069362f545a413539ef78c6f91efa6
lioc3sar <juliocesaoguimaraes@gmail.com>
2022-01-16 18:39:21 -0300

    Pagination

--- frontend/src/pages/listing/index.tsx
+++ frontend/src/pages/listing/index.tsx
@@ -30,7 +30,6 @@ function Listing() {
       });
   }, [pageNumber]);
   
-  // Example
   // const movie = {
   //   id: 1,
   //   image:
@@ -40,9 +39,13 @@ function Listing() {
   //   score: 4.5,
   // };
 
+  const handlePageChange = (newPageNumber: number) => {
+    setPageNumber(newPageNumber);
+  }
+
   return (
     <>
-      <Pagination />
+      <Pagination page={page} onChange={handlePageChange} />
       <div className="container">
         <div className="row">
           {page.content.map((movie) => {
